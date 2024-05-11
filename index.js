31d44702ccf38d81682c83a559058afc420fc4d0
cyughsd <gs1hatabobo@hotmail.com>
2024-05-11 21:52:40 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function add(x, y) {
-  return x + y;
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
 }
-
-const result = add(5, 3);
-console.log(result); // 8
