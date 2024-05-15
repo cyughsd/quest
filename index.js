a4d9b9de861f4a3b224afd2ade43f74063c9026b
cyughsd <gs1hatabobo@hotmail.com>
2024-05-15 15:46:58 +0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-function productExceptSelf(nums) {
-  const result = [];
-  let product = 1;
-  for (let i = 0; i < nums.length; i++) {
-    result[i] = product;
-    product *= nums[i];
+function longestPalindromeSubseq(s) {
+  const n = s.length;
+  const dp = Array.from(Array(n), () => Array(n).fill(0));
+  for (let i = n - 1; i >= 0; i--) {
+    dp[i][i] = 1;
+    for (let j = i + 1; j < n; j++) {
+      if (s[i] === s[j]) dp[i][j] = dp[i + 1][j - 1] + 2;
+      else dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+    }
   }
-  product = 1;
-  for (let i = nums.length - 1; i >= 0; i--) {
-    result[i] *= product;
-    product *= nums[i];
-  }
-  return result;
+  return dp[0][n - 1];
 }
